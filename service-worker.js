e6fd7c7aa8847c82190ee00fbac2a7af7e2fd5dc
khc7757-sys <khc7757@gmail.com>
2026-09-06 07:17:21 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE='paint-assembly-field-v2';
+﻿const CACHE='paint-assembly-field-v4-pending';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-192-maskable.png','./icon-512-maskable.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
